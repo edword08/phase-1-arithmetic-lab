519545a4df76f7adc61021ecf24af321bdf4793f
Eddy Matheson <rmatheson95@icloud.com>
2023-03-20 16:05:10 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 1000) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+//how does 16/6 = 4?
+const max = Math.max(5, 10, 15, 20);
